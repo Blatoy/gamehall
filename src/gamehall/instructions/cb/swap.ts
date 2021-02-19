d0c7156325fea4c0de0fc5f8f1d4fec779040e2c
Blatoy <lock.jeanneret@gmail.com>
2021-02-20 00:54:11 +0100

    Implement SWAP instruction

--- src/gamehall/instructions/cb/swap.ts
+++ src/gamehall/instructions/cb/swap.ts
@@ -1,45 +1,55 @@
 import { CPU } from "../../cpu.js";
-import { InstructionDefinition, NotImplementedError } from "../../instruction.js";
+import { InstructionDefinition, InstructionExecuteOutput } from "../../instruction.js";
+import { Pointer8 } from "../../pointer.js";
+
+function swap(cpu: CPU, value: Pointer8, clockCycles = 2): InstructionExecuteOutput {
+    const high = 0b1111_0000 & value.getUint();
+    value.setUint((value.getUint() << 4) | (high >> 4));
+    cpu.flags.reset();
+    cpu.flags.z.setValue(value.getUint() === 0);
+
+    return { clockCycles };
+}
 
 export default [
     {
         code: 0x30,
         name: 'SWAP b',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.b)
     },
     {
         code: 0x31,
         name: 'SWAP c',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.c)
     },
     {
         code: 0x32,
         name: 'SWAP d',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.d)
     },
     {
         code: 0x33,
         name: 'SWAP e',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.e)
     },
     {
         code: 0x34,
         name: 'SWAP h',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.h)
     },
     {
         code: 0x35,
         name: 'SWAP l',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.l)
     },
     {
         code: 0x36,
         name: 'SWAP (hl)',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.pointerHL8(), 4)
     },
     {
         code: 0x37,
         name: 'SWAP a',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => swap(cpu, cpu.registers.a)
     }
 ] as InstructionDefinition[];
