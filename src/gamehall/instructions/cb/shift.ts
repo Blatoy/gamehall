bc132a4a7760a649193e5d1babd705592ebfe9f9
Blatoy <lock.jeanneret@gmail.com>
2021-02-25 21:39:50 +0100

    Add SLA

--- src/gamehall/instructions/cb/shift.ts
+++ src/gamehall/instructions/cb/shift.ts
@@ -1,46 +1,59 @@
+import { getBit } from "../../../binch/utils.js";
 import { CPU } from "../../cpu.js";
-import { InstructionDefinition, NotImplementedError } from "../../instruction.js";
+import { InstructionDefinition, InstructionExecuteOutput, NotImplementedError } from "../../instruction.js";
+import { Pointer8 } from "../../pointer.js";
+
+/** Shift left. */
+function shiftLeft(cpu: CPU, register: Pointer8, machineCycles = 2): InstructionExecuteOutput {
+    const value = register.getUint();
+    cpu.flags.reset();
+    cpu.flags.c.setValue(getBit(value, 7));
+    register.setUint((value & 0b0111_1111) << 1);
+    cpu.flags.z.compute(value);
+
+    return { machineCycles };
+}
 
 export default [
     {
         code: 0x20,
         name: 'SLA b',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.b)
     },
     {
         code: 0x21,
         name: 'SLA c',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.c)
     },
     {
         code: 0x22,
         name: 'SLA d',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.d)
     },
     {
         code: 0x23,
         name: 'SLA e',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.e)
     },
     {
         code: 0x24,
         name: 'SLA h',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.h)
     },
     {
         code: 0x25,
         name: 'SLA l',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.l)
     },
     {
         code: 0x26,
         name: 'SLA (hl)',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.pointerHL8(), 4)
     },
     {
         code: 0x27,
         name: 'SLA a',
-        execute: (cpu: CPU) => { throw new NotImplementedError(); }
+        execute: (cpu: CPU) => shiftLeft(cpu, cpu.registers.a)
     },
     /** Shift right (keep bit 0*/
     {
